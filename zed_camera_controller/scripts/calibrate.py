76366347d74eb6c4f8fe0b2aa44a9e69daa4d735
ciccio42 <francescorosa97@virgilio.it>
2024-12-16 12:34:08 +0100

    update calibration procedure

--- zed_camera_controller/scripts/calibrate.py
+++ zed_camera_controller/scripts/calibrate.py
@@ -6,6 +6,9 @@ from cv_bridge import CvBridge
 import numpy as np
 from aruco_detector.srv import ArucoDetection, ArucoDetectionResponse
 
+Target_Position =  [0.022843813138628592, -0.43800020977692405, 0.5643843146648674]
+Target_Orientation = [0.3603325062389276, 0.015749675284185274, -0.0008269422755895826, 0.9326905965230317]
+
 if __name__ == "__main__":
     i = 0
     show_image = True
@@ -30,3 +33,24 @@ if __name__ == "__main__":
         aruco_pose = get_aruco_pose()
         rospy.loginfo(f"Camera pose wrt table_1 frame \n {aruco_pose}")
         rate.sleep()
+
+        frames = get_frames_client()
+        color_frame = frames.color_frames[0]
+
+        # convert message to cv2 image
+        color_cv_image = bridge.imgmsg_to_cv2(
+            color_frame, desired_encoding='rgba8')
+        cv2.imshow("Color", color_cv_image)
+        cv2.waitKey(100)
+        # cv2.destroyAllWindows()
+        
+        # 
+        position = aruco_pose.camera_pose.position
+        orientation = aruco_pose.camera_pose.orientation
+        
+        # compute difference
+        position_diff = np.array([position.x, position.y, position.z]) - np.array(Target_Position)
+        orientation_diff = np.array([orientation.x, orientation.y, orientation.z, orientation.w]) - np.array(Target_Orientation)
+        if (position_diff < 0.001).all() and (orientation_diff < 0.001).all():
+            rospy.loginfo("Calibration done")
+            
